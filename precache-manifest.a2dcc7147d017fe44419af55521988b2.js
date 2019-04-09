9bf18c1e3a50b85f065af6bba88bbbdbde6c81dc
Omar Gaston <omar.gaston.c@gmail.com>
2019-04-09 15:38:52 -0400

    Updates

---
+++ precache-manifest.a2dcc7147d017fe44419af55521988b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c3168e894aac8f0ee96",
+    "url": "/ogaston/math-game/static/js/runtime~main.791afaf6.js"
+  },
+  {
+    "revision": "f2d68be6d9dadb3037e0",
+    "url": "/ogaston/math-game/static/js/main.5fc0615a.chunk.js"
+  },
+  {
+    "revision": "14c362e523073f23c7b5",
+    "url": "/ogaston/math-game/static/js/2.5bb601e8.chunk.js"
+  },
+  {
+    "revision": "f2d68be6d9dadb3037e0",
+    "url": "/ogaston/math-game/static/css/main.e692df94.chunk.css"
+  },
+  {
+    "revision": "8e7318951fd5bd42ef550c654921b12d",
+    "url": "/ogaston/math-game/index.html"
+  }
+];
