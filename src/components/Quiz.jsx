663dd1d42f4f62ceb9801b3cb56cac704f97529e
ogaston <dominican_hot04@hotmail.com>
2019-04-18 17:03:11 -0400

    add new features of triple operations

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,125 +1,140 @@
-import React from 'react';
-import AnswerModal from './AnswerModal';
+import React from "react";
+import AnswerModal from "./AnswerModal";
 
 class Quiz extends React.Component {
+  _isMounted = false;
+  _secondsIntervalRef;
 
-    _isMounted = false;
-    _secondsIntervalRef;
+  state = {
+    problem: "",
+    answer: 0,
+    modal: "",
+    modalShowing: false
+  };
 
-    state = {
-        problem: "",
-        answer: 0,
-        modal: "",
-        modalShowing: false
-    };
+  correctAnswer = () => {
+    this.showModal("sucess");
+    this._isMounted && this.props.onCorretAnswer();
+    this.nextProblem();
+  };
 
-    correctAnswer = () => {
-        this.showModal("sucess");
-        this._isMounted && this.props.onCorretAnswer()
-        this.nextProblem()
-    };
+  componentDidMount() {
+    this._isMounted = true;
+    this.generateAnotherProblem();
+    this.answerInput.focus();
+  }
 
-    componentDidMount() {
-        this._isMounted = true;
-        this.generateAnotherProblem();
-        this.answerInput.focus();
+  shouldComponentUpdate(nextProps) {
+    if (this.props.lives < 1) {
+      this.props.onEndGame(this.state.points);
+      return false;
     }
+    return nextProps.lives > -1;
+  }
 
-    shouldComponentUpdate(nextProps) {
-        if (this.props.lives < 1) {
-            this.props.onEndGame(this.state.points)
-            return false;
-        }
-        return nextProps.lives > -1;
-    }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
+  wrongAnswer = () => {
+    this._isMounted && this.props.onWrongAnswer();
 
-    wrongAnswer = () => {
-        this._isMounted && this.props.onWrongAnswer();
+    this.showModal("error");
+    this.nextProblem();
+  };
 
-        this.showModal("error");
-        this.nextProblem()
-    };
-
-    nextProblem = () => {
-        setTimeout(() => {
-            this.generateAnotherProblem()
-            this._isMounted && this.setState({
-                modalShowing: false
-            });
-            if (this.props.lives > 0) this.answerInput.focus();
-        }, 2500)
-    }
-
-    evaluateProblem = () => {
-
-        // eslint-disable-next-line no-eval
-        if (eval(this.state.problem) === +this.state.answer) {
-            return this.correctAnswer();
-        }
-
-        return this.wrongAnswer();
-    };
-
-    keyingUp = ev => {
-        if (ev.key === "Enter") {
-            this.evaluateProblem();
-        }
+  nextProblem = () => {
+    setTimeout(() => {
+      this.generateAnotherProblem();
+      this._isMounted &&
         this.setState({
-            answer: ev.target.value
+          modalShowing: false
         });
-    };
+      if (this.props.lives > 0) this.answerInput.focus();
+    }, 2500);
+  };
 
-    showModal = (type) => {
-        this.setState({
-            modal: ( <AnswerModal type={type} />),
-            modalShowing: true
-        });
-    };
-
-    getRandomSymbol = () => {
-        const symbols = ["+", "-", "/", "*"];
-        var min = 0;
-        var max = 3;
-        var random = Math.floor(Math.random() * (+max - +min) + +min);
-        return symbols[random];
+  evaluateProblem = () => {
+    // eslint-disable-next-line no-eval
+    if (eval(this.state.problem) === +this.state.answer) {
+      return this.correctAnswer();
     }
 
-    generateAnotherProblem = () => {
-        const newProblem = `${(Math.random() * 100).toFixed()} ${this.getRandomSymbol()} ${(Math.random() * 100).toFixed()}`;
-        this._isMounted && this.setState({
-            problem: newProblem
-        });
-    };
+    return this.wrongAnswer();
+  };
 
-    render() {
-        return ( 
-        <section>
-            <div> 
-            {this.state.modalShowing ? 
-                (
-                    this.state.modal
-                ) : ( 
-                    <div>
-                        <h1 style={{ fontSize: "3.5em"}}> {this.state.problem} </h1> 
-                        <input 
-                            ref={(input) => { this.answerInput = input; }}
-                            className = "App-input"
-                            type = "text"
-                            placeholder = "Respuesta"
-                            onKeyUp = {
-                                this.keyingUp
-                            }/> 
-                    </div>
-                )
-            }
-            </div> 
-        </section>
-        );
+  keyingUp = ev => {
+    if (ev.key === "Enter") {
+      this.evaluateProblem();
     }
+    this.setState({
+      answer: ev.target.value
+    });
+  };
+
+  showModal = type => {
+    this.setState({
+      modal: <AnswerModal type={type} />,
+      modalShowing: true
+    });
+  };
+
+  getRandomSymbol = () => {
+    const symbols = ["+", "-", "/", "*"];
+    var min = 0;
+    var max = 3;
+    var random = Math.floor(Math.random() * (+max - +min) + +min);
+    return symbols[random];
+  };
+
+  generateAnotherProblem = () => {
+    const { level } = this.props;
+    const problem = [this.getRandomNumbers()];
+
+    for (let i = 0; i < level; i += 5) {
+      let symb = this.getRandomSymbol();
+      let num =
+        symb == "/" || symb == "*"
+          ? this.getRandomNumbers(10)
+          : this.getRandomNumbers();
+      problem.push(symb, num);
+    }
+
+    const newProblem = problem.join(" ");
+    this._isMounted &&
+      this.setState({
+        problem: newProblem
+      });
+  };
+
+  getRandomNumbers = num => {
+    return (Math.random() * (num || 100)).toFixed();
+  };
+
+  render() {
+    return (
+      <section>
+        <div>
+          {this.state.modalShowing ? (
+            this.state.modal
+          ) : (
+            <div>
+              <h1 style={{ fontSize: "3.5em" }}> {this.state.problem} </h1>
+              <input
+                ref={input => {
+                  this.answerInput = input;
+                }}
+                className="App-input"
+                type="text"
+                placeholder="Respuesta"
+                onKeyUp={this.keyingUp}
+              />
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Quiz;
