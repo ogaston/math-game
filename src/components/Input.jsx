8cb88b036e0a09107b681b5baca34d5de1b97fa9
Omar Yei Gaston Chalas <ogaston@adm.unapec.edu.do>
2019-08-19 20:38:32 -0400

    adding PWA capabilities

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 export default function Input({ type, text, onInputChange }) {
     return (
-        <input 
-            type={ type || "text" } 
-            className="App-input App-input--sm" 
-            placeholder={text} 
-            onChange={onInputChange}
-        />
+        <>
+            <label for="textId" hidden>{"Text"}</label>
+            <input 
+                id="textId"
+                type={ type || "text" } 
+                className="App-input App-input--sm" 
+                placeholder={text} 
+                onChange={onInputChange}
+            />
+        </>
     )
 }
